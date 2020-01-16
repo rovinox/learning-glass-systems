a8275d7adb71d91a1f049ac61118e4f32836eede
neaj mahmud <neajmahmud230@gmail.com>
2020-01-16 12:27:32 -0500

    done with contact

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -3,6 +3,8 @@ import Container from "@material-ui/core/Container"
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Alert from '@material-ui/lab/Alert';
+import Snackbar from '@material-ui/core/Snackbar';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -28,31 +30,84 @@ const useStyles = makeStyles(theme => ({
 export default function ContactUs() {
    
     const classes = useStyles();
-    const [input, setInput] = React.useState("")    
+    const [fullName, setFullName] = React.useState("")
+    const [email, setEmail] = React.useState("")
+    const [message, setMessage] = React.useState("") 
+    const [success , setSuccess ] = React.useState(false); 
+    const [fail , setFail] = React.useState(false);
+    
+    const handleSuccess  = () => {
+        setSuccess(true);
+      };
 
-    const handleInput = event => {
+    const handleSuccessClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setSuccess(false);
+      }; 
+      
+      const handleFail  = () => {
+        setFail(true);
+      };
+
+    const handleFailClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setFail(false);
+      };  
+
+
+    const handleMessage = event => {
         const { name, value } = event.target;
-        setInput({[name]: value });
+        setMessage({[name]: value });
     };
+
+    const handleName = event => {
+        const { name, value } = event.target;
+        setFullName({[name]: value });
+    }
+
+    const handleEmail = event => {
+        const { name, value } = event.target;
+        setEmail({[name]: value });
+    }
 
     const submitInfo = (e) => {
         e.preventDefault()
-        console.log(input);
+        if(!fullName|| !email || !message) {
+            handleFail()
+        } else {
+            handleSuccess()
+             }
     }   
 
         return (
             
             <div>
+                <Snackbar open={success} autoHideDuration={6000} onClose={handleSuccessClose}>
+                  <Alert variant="filled" onClose={handleSuccessClose} severity="success">
+                   Your Message Has Been Sent
+                  </Alert>
+                </Snackbar>
+                <Snackbar open={fail} autoHideDuration={6000} onClose={handleFailClose}>
+                  <Alert variant="filled" onClose={handleFailClose} severity="error">
+                   Please fill out all the fields
+                  </Alert>
+                </Snackbar>
                 <form className={classes.root} onSubmit={submitInfo} >
                     <TextField
-                    onChange={handleInput}
+                    onChange={handleName}
                     className={classes.textFieldSize}
                     name="name"
                     label="Full Name"
                     variant="outlined" />
                     
                     <TextField
-                    onChange={handleInput}
+                    onChange={handleEmail}
                     className={classes.textFieldSize}
                     name="email"
                     label="Your Email"
@@ -61,7 +116,7 @@ export default function ContactUs() {
                     />
 
                     <TextField
-                    onChange={handleInput}
+                    onChange={handleMessage}
                     className={classes.textFieldSize}
                     name="message"
                     label="Message"
@@ -70,6 +125,7 @@ export default function ContactUs() {
                     variant="outlined" />
                     <Button className={classes.textFieldSize} type="submit" variant="contained" color="primary">send</Button>
                 </form>
+                
             </div>
         )
     
