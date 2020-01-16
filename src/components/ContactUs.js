9ec1b58f9e5415320fc097054c2c9ebde2bbb6b6
neaj mahmud <neajmahmud230@gmail.com>
2020-01-15 23:59:36 -0500

    working on contact

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,17 +2,23 @@ import React from 'react'
 import Container from "@material-ui/core/Container"
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
     root: {
       display: 'flex',
       flexWrap: 'wrap',
-      height:"800px",
-      width:"800px",
-      border: "20px red solid"
+      flexDirection:"column",
+      alignItems:"center",
+      marginTop:"100px"
+      
     },
     textFieldSize: {
-        width: "400px"
+        width: "400px",
+        marginTop:"20px"
+    },
+    botton:{
+        marginTop:"30px"
     }
     
   }));
@@ -20,13 +26,50 @@ const useStyles = makeStyles(theme => ({
 
 
 export default function ContactUs() {
+   
     const classes = useStyles();
+    const [input, setInput] = React.useState("")    
+
+    const handleInput = event => {
+        const { name, value } = event.target;
+        setInput({[name]: value });
+    };
+
+    const submitInfo = (e) => {
+        e.preventDefault()
+        console.log(input);
+    }   
+
         return (
-            <div className={classes.root}>
-                <TextField
+            
+            <div>
+                <form className={classes.root} onSubmit={submitInfo} >
+                    <TextField
+                    onChange={handleInput}
                     className={classes.textFieldSize}
+                    name="name"
                     label="Full Name"
                     variant="outlined" />
+                    
+                    <TextField
+                    onChange={handleInput}
+                    className={classes.textFieldSize}
+                    name="email"
+                    label="Your Email"
+                    variant="outlined" 
+                    type="email"
+                    />
+
+                    <TextField
+                    onChange={handleInput}
+                    className={classes.textFieldSize}
+                    name="message"
+                    label="Message"
+                    multiline
+                    rows="6"
+                    variant="outlined" />
+                    <Button className={classes.textFieldSize} type="submit" variant="contained" color="primary">send</Button>
+                </form>
             </div>
         )
     
